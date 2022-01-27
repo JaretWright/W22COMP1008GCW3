c942d0726a6d1267dac76deed971236a67a50b6c
JaretWright <jaret.wright@georgiancollege.ca>
2022-01-27 14:04:13 -0500

    Integrated the controller and the view objects to display a Card

--- src/main/java/com/example/w22comp1008gcw3/CardViewController.java
+++ src/main/java/com/example/w22comp1008gcw3/CardViewController.java
@@ -1,4 +1,44 @@
 package com.example.w22comp1008gcw3;
 
-public class CardViewController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class CardViewController implements Initializable {
+
+    @FXML
+    private Label cardLabel;
+
+    @FXML
+    private Label faceNameLabel;
+
+    @FXML
+    private Label suitLabel;
+
+    @FXML
+    private Label cardValueLabel;
+
+    private DeckOfCards deck;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        //create a DeckOfCards
+        deck = new DeckOfCards();
+        viewNextCard();
+    }
+
+    @FXML
+    private void viewNextCard() {
+        Card card = deck.dealTopCard();
+
+        //update the Labels with the Card information
+        cardLabel.setText(card.toString());
+        cardValueLabel.setText(Integer.toString(card.getFaceValue()));
+        faceNameLabel.setText(card.getFaceName());
+        suitLabel.setText(card.getSuit());
+    }
 }
